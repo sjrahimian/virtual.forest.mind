697461678d05a98fa5a16f5bc918bd132ce44490
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2025-10-03 12:06:45 -0400

    License added.

--- vfm/vfm.py
+++ vfm/vfm.py
@@ -3,10 +3,24 @@
 """
 vfm.py — Virtual Forest Mind CLI Tool
 
-Provides commands to create, search, and open Markdown notes in user-defined spaces, 
-  using a configuration file for paths and editor settings.
+    Provides commands to create, search, and open Markdown notes in user-defined spaces, 
+    using a configuration file for paths and editor settings.
 
-Created: 2025-09-30
+    Copyright (C) 2025  Sama Rahimian
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 
 __version__ = "0.1.1"
